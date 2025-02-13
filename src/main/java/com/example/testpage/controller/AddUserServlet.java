d6b0a8fea1696a800bdcfb1e61c53d675a2c4216
Tigran Sarukhanyan <112509401+TigranSar@users.noreply.github.com>
2025-02-13 22:41:33 +0400

    Update AddUserServlet.java
    fixed bugs

--- src/main/java/com/example/testpage/controller/AddUserServlet.java
+++ src/main/java/com/example/testpage/controller/AddUserServlet.java
@@ -20,12 +20,6 @@ public class AddUserServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init();
     }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
